88d7a33ba374215dc7321cc7f0df5d6cf2964d72
NIne Health Team <ninehealth@outlook.com>
2022-08-10 22:59:25 +0700

    Using Axios to call API from Django back-end

--- src/screens/components/list_view.js
+++ src/screens/components/list_view.js
@@ -1,52 +1,76 @@
 import React, { Component } from "react";
-import { StyleSheet, SafeAreaView, Image, Text, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Image, Text, Button, FlatList } from "react-native";
+import client from "./../../api/client";
 // import { IMAGENAME } from "./src/image/index";
 
 class ListView extends Component {
-	render() {
-	  const mytext = "Good health, good life !";
-	  return (
-	    <SafeAreaView style={styles.center}>
-	      <Image
-        	style={styles.pizzaImage}
-        	source={require('./9Health.png')  }
-	      />
-	      <Text style={styles.baseText}>9Health</Text>
-	      <Text style={styles.newText}>{mytext}</Text>
-	      <Text style={styles.title}>List View</Text>
-	      <Button 
-	      	title="list Item, Click for Details"
-	      	onPress= {() => this.props.navigation.navigate("Detail")}
-	      />
-	    </SafeAreaView>
-	  );
+    constructor(props) {
+	    super(props);
+	    this.state = {
+		    data:[],
+	    };
+    }
+
+	componentDidMount() {
+		client.get("/").then((response) => {
+			this.setState({ data: response.data });
+		});
 	}
-}
 
-const styles = StyleSheet.create({
-	center: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	title: {
-		fontSize: 36,
-		marginBottom: 16,
-	},
-	baseText: {
-        color: "navy",
-        fontSize: 40,
-        //fontStyle: "italic",
-    },
-    newText: {
-        color: "red",
-        fontStyle: "italic",
-        fontSize: 20
-    },
-    Pizzaimage: {
-        width: 200,
-        height: 200,
-    },  
-});
+    render() {
+    const { data } = this.state;  
+    const mytext = "Good Health, good life"
+    return (
+    <SafeAreaView style={styles.container}>
+    <Image
+        style={styles.pizzaImage}
+        source={ require('./9Health.png') } 
+      />
+      <Text style={styles.baseText}>NineHealth</Text>
+      <Text style={styles.newText}>{mytext}</Text>
+      <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+          	<Text style={styles.itemText}>
+              {item.pizzeria_name}, {item.city}
+            </Text>
+          	) }
+        />
+      <Button
+          title="list Item, Click for Details"
+          onPress={() => this.props.navigation.navigate("Detail")}
+        />
+    </SafeAreaView>
+    );
+    }
+  }
 
-export default ListView;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    baseText: {
+      color: "navy",
+      fontSize: 30,
+      fontStyle: "italic",
+    },
+    newText:{
+      color: "red",
+    },
+    pizzaImage: {
+      width: 200,
+      height: 200,
+    },
+    itemText: {
+        color: "green",
+        fontSize: 20,
+      }
+  })
+  
+
+  export default ListView;
+
